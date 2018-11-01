0da10be4a2fe8c6ddd2eca998bec75c78cfb7cfb
mazda4952 <one.sonb.son@gmail.com>
2018-10-31 19:23:20 -0700

    can namespace fixed

--- lib/can.cpp
+++ lib/can.cpp
@@ -6,8 +6,16 @@
  */
 
 #include "can.h"
+#include "fsl_flexcan.h"
+#include "fsl_clock.h"
 
-using namespace BSP;
+using namespace BSP::CAN;
+
+struct CAN::controller_data {
+	flexcan_handle_t flexcanHandle;
+};
+
+static void flexcan_callback(CAN_Type* base, flexcan_handle_t* handle, status_t status, uint32_t result, void* userData);
 
 CAN::CAN(const config* conf){
 
@@ -19,13 +27,59 @@ void CAN::tick() {
 
 void CAN::init() {
 	// HW init
+
+
 }
 
-bool CAN::txMSG(const mb_frame&) {
+bool CAN::txMSG(const mb_info& frame) {
 	return false;
 }
 
-bool CAN::rxMSG(const mb_frame&, const callback_type&) {
+bool CAN::rxMSG(const mb_info&, const callback_type&) {
 	return false;
+}
+
+void CAN::config_controller(CAN_Type* base) {
+	flexcan_config_t flexcanConfig;
+
+	/* Get FlexCAN module default Configuration. */
+	/*
+	 * flexcanConfig.clkSrc = kFLEXCAN_ClkSrcOsc;
+	 * flexcanConfig.baudRate = 1000000U;
+	 * flexcanConfig.maxMbNum = 16;
+	 * flexcanConfig.enableLoopBack = false;
+	 * flexcanConfig.enableSelfWakeup = false;
+	 * flexcanConfig.enableIndividMask = false;
+	 * flexcanConfig.enableDoze = false;
+	 * flexcanConfig.timingConfig = timingConfig;
+	 */
+	FLEXCAN_GetDefaultConfig(&flexcanConfig);
+
+	flexcanConfig.enableIndividMask = true;
+
+	// select clock source and get freq
+	flexcan_clock_source_t clk_src;
+	clock_name_t clockName;
+	if ( /*config value*/0 == clock_source::BUS_CLK) {
+		clk_src = kFLEXCAN_ClkSrcPeri;
+		clockName = kCLOCK_BusClk;
+	} else {
+		clk_src = kFLEXCAN_ClkSrcOsc;
+		clockName = kCLOCK_ScgSysOscAsyncDiv2Clk;
+	}
+
+	uint32_t clock_source_freq = CLOCK_GetFreq(clockName);
+
+	flexcanConfig.clkSrc = clk_src;
+
+	FLEXCAN_Init(CAN0, &flexcanConfig, clock_source_freq);
+
+	/* Create FlexCAN handle structure and set call back function. */
+	FLEXCAN_TransferCreateHandle(CAN0, &flexcanHandle, flexcan_callback, NULL);
+}
+
+//(*flexcan_transfer_callback_t)(CAN_Type *base, flexcan_handle_t *handle, status_t status, uint32_t result, void *userData);
+static void flexcan_callback(CAN_Type *base, flexcan_handle_t *handle, status_t status, uint32_t result, void *userData) {
+	// TODO
 }
 
--- lib/can.h
+++ lib/can.h
@@ -13,65 +13,101 @@
 #include "Service.h"
 #include "Event.h"
 
+#include <vector>
+
 namespace BSP {
+namespace CAN {
+
+using callback_type = se::Event<void(const struct mb_info*)>;
+
+class mb_info {
+public:
+	enum class mb_transfer_mode {
+		TX,
+		RX
+	} mode;
+
+	callback_type callback;
+
+	uint32_t 	id;
+	uint8_t 	data[8];
+	uint8_t 	dlc;
+	bool 		remote;
+	bool 		extended_id;
+
+	mb_info();
+
+	struct flexcan_frame_t* initialize_frame(struct flexcan_frame_t*);
+};
+
+struct controller_config {
+
+	enum class clock_source {
+		SOSCDIV2_CLK,		// SOSCDIV2_CLK clock source
+		BUS_CLK				// Peripheral clock source from PCC
+	} clock = clock_source::BUS_CLK;
+
+	uint32_t baudrate = 1000000;
+	bool enableLoopback = false;
+
+	controller_config() = default;
+};
 
 class CAN final : public StaticService<CAN, const struct config*> {
 public:
 
 	struct config {
 		// input parameters
-		// flags for can0 and can1 base
+		// can0 and can1 configs
 		//
+		struct controller_config* CAN0_config;
+		struct controller_config* CAN1_config;
 	};
 
-	struct controller_config {
-		uint32_t baudrate;
-	};
-
-	struct mb_config {
-		// mode
-	};
-
-	struct mb_frame {
-		uint16_t id;
-		uint8_t data[8];
-		uint8_t dlc;
-	};
-
-	using callback_type = se::Event<void(const mb_config&)>;
-
-	void tick() override {}
-	void init() override {}
+	void tick() override;
+	void init() override;
 
 	CAN(const config*);
 
 	/*
 	 * configure mb with rx or tx settings
 	 */
-	uint8_t setMBConfig(const mb_config*);
+	uint8_t setMBConfig(/* MB Settings */);
 
 	/*
-	 * tx using selected mb
+	 * tx using any mb
 	 */
-	bool txMSG(const mb_frame&);
+	bool txMSG(const mb_info& frame);
+
+	/*
+	 *
+	 */
+	bool txMSG(uint32_t id, const uint8_t data[], uint8_t dlc, bool remote,
+			bool extended_id, const callback_type* callback = nullptr);
 
 	/*
 	 * rx using selected mb
 	 */
-	bool rxMSG(const mb_frame&, const callback_type& callback);
+	bool rxMSG(const mb_info& frame, const callback_type& callback);
 
 private:
 
-	struct mb_data {
-		volatile mb_frame frame_data;
-		mb_config config_data;
-	};
+	struct controller_data;
+
+	// parameters passed in during construction, used during init
+	controller_data CAN0_config;
+	controller_data CAN1_config;
+
+	std::vector<mb_info> mb_can0;
+	std::vector<mb_info> mb_can1;
 
 	CAN() = default;
 
+	void config_controller(CAN_Type* base);
 
 };
 
+}
 }
 
 
