c256de8e7ff853fae6d0d134329c3c73d48e937c
mazda4952 <one.sonb.son@gmail.com>
2018-11-02 10:47:43 -0700

    formatting fixed

--- lib/i2c.h
+++ lib/i2c.h
@@ -2,15 +2,17 @@
 
 namespace BSP {
 namespace I2C {
-struct I2Cconfig{}
+
+struct I2Cconfig {
+};
+
 class I2C: public StaticService<I2C, const I2Cconfig&> {
 public:
 	I2C(const I2Cconfig&);
 
 	void tick() override;
 	void init() override;
-}
+};
 
-}
 }
 }
