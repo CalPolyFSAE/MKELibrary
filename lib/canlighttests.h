a97525bb892c2c96733be97ada5f7ecb1b9acafb
salticids <graydon@seewasp.com>
2019-03-02 17:31:59 -0800

    Motor controller test

--- lib/canlighttests.h
+++ lib/canlighttests.h
@@ -1,26 +1,26 @@
 #include "canlight.h"
 
-using namespace BSP::can;
+using namespace BSP;
 
 uint8_t counter;
 
 void cb(void){
-	CANlight::frame f = CANlight::StaticClass().readrx(1);
+	can::CANlight::frame f = can::CANlight::StaticClass().readrx(1);
 	if(f.data[0] == 0xff) counter++;
 }
 
 void loopbacktest(){
 
-	canlight_config c;
-	CANlight::ConstructStatic(&c);
-	CANlight& can = CANlight::StaticClass();
+	can::canlight_config c;
+	can::CANlight::ConstructStatic(&c);
+	can::CANlight& can = can::CANlight::StaticClass();
 
-	CANlight::canx_config cx;
+	can::CANlight::canx_config cx;
 	can.init(0, &cx);
 	cx.callback = cb;
 	can.init(1, &cx);
 
-	CANlight::frame f;
+	can::CANlight::frame f;
 	f.data[0] = 0xff;
 	f.ext = 1;
 	for(uint8_t i = 0; i < 50; i++){
@@ -29,4 +29,33 @@ void loopbacktest(){
 
     while(1);
 
+}
+
+// Test motor controller response
+// Attempts to read CAN offset
+can::CANlight::frame out;
+
+void callback(){
+    can::CANlight& can = can::CANlight::StaticClass();
+    can::CANlight::frame f = can.readrx(0);
+
+    if(f.id == 0x222)
+    PRINTF("0x%x 0x%x\n", f.data[4], f.data[5]);
+}
+
+void motorcontrollertest(){
+
+    can::canlight_config config;
+    can::CANlight::canx_config can0_config;
+    can::CANlight::ConstructStatic(&config);
+    can::CANlight& can = can::CANlight::StaticClass();
+    can0_config.baudRate = 500000;
+    can0_config.callback = callback;
+    can.init(0, &can0_config);
+
+    out.id = 0x221;
+    out.ext = 1;
+    out.data[0] = 141;
+
+    can.tx(0, out);
 }
