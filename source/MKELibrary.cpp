db87c83b6b00e4fb8d559cb2ce976f4ab4410f8c
TheYoshiStory <AyusmanSaha@gmail.com>
2019-05-09 00:30:50 -0700

    CAN forwarder for charger

--- source/MKELibrary.cpp
+++ source/MKELibrary.cpp
@@ -1,29 +1,67 @@
-#include <stdio.h>
-#include "board.h"
-#include "peripherals.h"
-#include "pin_mux.h"
-#include "clock_config.h"
 #include "MKE18F16.h"
-#include "fsl_debug_console.h"
+#include "clock_config.h"
+#include "pin_mux.h"
+
+#include "gpio.h"
 #include "canlight.h"
-#include "canlighttests.h"
+
+#define GEN_CAN_BUS 0
+#define GEN_CAN_BAUD_RATE 500000
+#define GEN_CAN_LED 15
+
+#define CHG_CAN_BUS 1
+#define CHG_CAN_BAUD_RATE 250000
+#define CHG_CAN_LED 16
 
 using namespace BSP;
 
-
-void tick(void){
-
+// forwards message to charger CAN bus
+static void gen_can_callback() {
+    gpio::GPIO &gpio = gpio::GPIO::StaticClass();
+    can::CANlight &can = can::CANlight::StaticClass();
+    gpio.toggle(gpio::PortD, GEN_CAN_LED);
+    can.tx(CHG_CAN_BUS, can.readrx(GEN_CAN_BUS));
 }
 
-int main(void) {
-    BOARD_InitBootPins();
+// forwards message to general CAN bus
+static void chg_can_callback() {
+    gpio::GPIO &gpio = gpio::GPIO::StaticClass();
+    can::CANlight &can = can::CANlight::StaticClass();
+    gpio.toggle(gpio::PortD, CHG_CAN_LED);
+    can.tx(GEN_CAN_BUS, can.readrx(CHG_CAN_BUS));
+}
+
+// main program
+int main() {
+    can::canlight_config config;
+    can::CANlight::canx_config canx_config;
+    
+    // initialize board hardware
     BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
-	BOARD_InitDebugConsole();
+    BOARD_InitBootPins();
+    
+    // initialize GPIO driver
+    gpio::GPIO::ConstructStatic();
+    gpio::GPIO &gpio = gpio::GPIO::StaticClass();
+    
+    // configure GPIO outputs
+    gpio.out_dir(gpio::PortD, GEN_CAN_LED);
+    gpio.out_dir(gpio::PortD, CHG_CAN_LED);
+    
+    // initialize CAN driver
+    can::CANlight::ConstructStatic(&config);
+    can::CANlight &can = can::CANlight::StaticClass();
+    
+    // configure general CAN bus
+    canx_config.baudRate = GEN_CAN_BAUD_RATE;
+    canx_config.callback = gen_can_callback;
+    can.init(GEN_CAN_BUS, &canx_config);
+    
+    // configure charger CAN bus
+    canx_config.baudRate = CHG_CAN_BAUD_RATE;
+    canx_config.callback = chg_can_callback;
+    can.init(CHG_CAN_BUS, &canx_config);
 
-    bursttest();
-
-    while(1) {
-    }
-    return 0;
+    while(true);
+    return(EXIT_FAILURE);
 }
