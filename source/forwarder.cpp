a24dfcffece3ec365a4d3330910c187458bfbece
TheYoshiStory <AyusmanSaha@gmail.com>
2019-05-09 00:50:10 -0700

    made CAN forwarder universal and in its own file

---
+++ source/forwarder.cpp
@@ -0,0 +1,49 @@
+#include "forwarder.h"
+#include "gpio.h"
+#include "canlight.h"
+
+using namespace BSP;
+
+// forwards message from CAN0 to CAN1
+static void can0_callback() {
+    gpio::GPIO &gpio = gpio::GPIO::StaticClass();
+    can::CANlight &can = can::CANlight::StaticClass();
+    gpio.toggle(gpio::PortD, CAN0_LED);
+    can.tx(1, can.readrx(0));
+}
+
+// forwards message from CAN1 to CAN0
+static void can1_callback() {
+    gpio::GPIO &gpio = gpio::GPIO::StaticClass();
+    can::CANlight &can = can::CANlight::StaticClass();
+    gpio.toggle(gpio::PortD, CAN1_LED);
+    can.tx(0, can.readrx(1));
+}
+
+// initializes the CAN forwarder
+void forwarder_init() {
+    can::canlight_config config;
+    can::CANlight::canx_config canx_config;
+    
+    // initialize GPIO driver
+    gpio::GPIO::ConstructStatic();
+    gpio::GPIO &gpio = gpio::GPIO::StaticClass();
+    
+    // configure GPIO outputs
+    gpio.out_dir(gpio::PortD, CAN0_LED);
+    gpio.out_dir(gpio::PortD, CAN1_LED);
+    
+    // initialize CAN driver
+    can::CANlight::ConstructStatic(&config);
+    can::CANlight &can = can::CANlight::StaticClass();
+    
+    // configure CAN0
+    canx_config.baudRate = CAN0_BAUD_RATE;
+    canx_config.callback = can0_callback;
+    can.init(0, &canx_config);
+    
+    // configure CAN1
+    canx_config.baudRate = CAN1_BAUD_RATE;
+    canx_config.callback = can1_callback;
+    can.init(1, &canx_config);
+}
---
+++ source/forwarder.h
@@ -0,0 +1,12 @@
+#ifndef FORWARDER_H
+#define FORWARDER_H
+
+#define CAN0_LED 15
+#define CAN0_BAUD_RATE 500000
+
+#define CAN1_LED 16
+#define CAN1_BAUD_RATE 250000
+
+void forwarder_init();
+
+#endif
